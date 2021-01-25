6d0e68ee6b96b3a7bde50d856181c14b869864e7
GioTeckOfficial <gio.addy77@gmail.com>
2021-01-25 12:58:40 +0100

    Fixed index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { generateKeyPair } = require("crypto");
 const ds=require("discord.js")
 const client=new ds.Client();
 const fs=require("fs");
@@ -15,7 +16,7 @@ client.on('ready',()=>{
 
 client.on('message',message=>{
     if(message.author.bot) return;
-    let latest=message.content.toLowerCase();
+    /*let latest=message.content.toLowerCase();
     let oldest=JSON.parse(fs.readFileSync(`./messages/${message.member.id}.json`,'utf-8'));
     oldest=oldest.msg.toLowerCase();
     fs.writeFileSync(`./messages/${message.member.id}.json`,`{"msg":"${message.content}"}`)
@@ -34,5 +35,28 @@ client.on('message',message=>{
             .setDescription(`${message.member} per favore non mandare sempre lo stesso messaggio!`)
             .setFooter(message.guild.name,message.guild.iconURL());
         message.reply(embed)
+    }*/
+    fs.readFile(`./messages/${message.member.id}.json`,{encoding:'utf-8'},(err,data)=>{
+        if(err) return fs.writeFileSync(`./messages/${message.member.id}.json`,`{"msg":"${message.content}"}`);
+        let latest=message.content.toLowerCase();
+        let oldest=JSON.parse(data).msg.toLowerCase();
+
+        fs.writeFileSync(`./messages/${message.member.id}.json`,`{"msg":"${message.content}"}`)
+
+    if(latest==oldest){
+        message.delete();
+        let embed=new ds.MessageEmbed()
+            .setTitle("NoSpam")
+            .setDescription(`${message.member} per favore non mandare sempre lo stesso messaggio!`)
+            .setFooter(message.guild.name,message.guild.iconURL());
+        message.reply(embed)
+    }else if(latest.includes(oldest)){
+        message.delete();
+        let embed=new ds.MessageEmbed()
+            .setTitle("NoSpam")
+            .setDescription(`${message.member} per favore non mandare sempre lo stesso messaggio!`)
+            .setFooter(message.guild.name,message.guild.iconURL());
+        message.reply(embed)
     }
+    });
 });
